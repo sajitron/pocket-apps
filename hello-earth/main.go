f435a35a175be78a269b11032ec74cf688f18de8
Adams Banjo <sajioloye@gmail.com>
2024-01-06 15:18:24 -0500

    use switch statement to print for different languages

--- hello-earth/main.go
+++ hello-earth/main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet()
+	greeting := greet("en")
 	fmt.Println(greeting)
 }
 
-// greet returns a greeting to the world.
-func greet() string {
-	// return a simple greeting message
-	return "Hello world"
+// language represents the language's code
+type language string
+
+// greet says hello to the world in the specified language
+func greet(l language) string {
+	switch l {
+	case "en":
+		return "Hello world"
+	case "fr":
+		return "Bonjour le monde"
+	default:
+		return ""
+	}
 }
--- hello-earth/main_internal_test.go
+++ hello-earth/main_internal_test.go
@@ -2,10 +2,41 @@ package main
 
 import "testing"
 
-func TestGreet(t *testing.T) {
+func ExampleMain() {
+	main()
+	// Output:
+	// Hello world
+}
+
+func TestGreet_English(t *testing.T) {
+	lang := language("en")
 	want := "Hello world"
 
-	got := greet()
+	got := greet(lang)
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_French(t *testing.T) {
+	lang := language("fr")
+	want := "Bonjour le monde"
+
+	got := greet(lang)
+
+	if got != want {
+		// mark this test as failed
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
+
+func TestGreet_Klingon(t *testing.T) {
+	lang := language("kl")
+	want := ""
+
+	got := greet(lang)
 
 	if got != want {
 		// mark this test as failed
