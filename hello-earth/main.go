7fafb20ccaabaa4de7c13d5b647837b7b3b76686
Adams Banjo <sajioloye@gmail.com>
2024-01-07 16:56:23 -0500

    accept command line inputs

--- hello-earth/main.go
+++ hello-earth/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	greeting := greet("en")
+	var lang string
+	flag.StringVar(&lang, "lang", "en", "The required language, e.g. en, ur...")
+	flag.Parse()
+
+	greeting := greet(language(lang))
 	fmt.Println(greeting)
 }
 
@@ -12,12 +19,13 @@ type language string
 
 // phrasebook holds greeting for each supported language
 var phrasebook = map[language]string{
-	"el": "Γειά σου κόσμο",    // Greek
-	"en": "Hello world",       // English
-	"fr": "Bonjour le monde",  // French
-	"he": "שלום עולם",         // Hebrew
-	"ur": "سلام دنیا",         // Urdu
-	"vi": "Xin chào thế giới", // Vietnamese
+	"el": "Γειά σου κόσμο",                   // Greek
+	"en": "Hello world",                      // English
+	"fr": "Bonjour le monde",                 // French
+	"he": "שלום עולם",                        // Hebrew
+	"ur": "سلام دنیا",                        // Urdu
+	"vi": "Xin chào thế giới",                // Vietnamese
+	"yr": "eyin temi nigboro, ki lon happen", // Yoruba
 }
 
 // greet says hello to the world in the specified language
