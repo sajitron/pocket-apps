44e53edcd7e150bbdf6a1a4c223a9c7283131960
Adams Banjo <sajioloye@gmail.com>
2024-01-10 18:34:49 -0500

    add bookworm structs

--- bookworms/bookworm.go
+++ bookworms/bookworm.go
@@ -1,3 +1,17 @@
+package main
+
+// A Bookworm contains the list of books on a bookworm's shelf.
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:"books"`
+}
+
+// Book describes a book on a bookworm's shelf.
+type Book struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
+}
+
 // loadBookworms reads the file and returns the list of bookworms, and their beloved books, found therein.
 func loadBookworms(filePath string) ([]Bookworm, error) {
 	return nil, nil
