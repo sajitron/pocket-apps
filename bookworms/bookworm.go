9ecd12a757b97eed48c40611116fe1485407c9b5
sajitron <adams.b@roserocket.com>
2024-01-10 17:20:21 -0500

    add bookworm project

---
+++ bookworms/bookworm.go
@@ -0,0 +1,4 @@
+// loadBookworms reads the file and returns the list of bookworms, and their beloved books, found therein.
+func loadBookworms(filePath string) ([]Bookworm, error) {
+	return nil, nil
+}
---
+++ bookworms/go.mod
@@ -0,0 +1,3 @@
+module github.com/sajitron/pocket-apps/bookworms
+
+go 1.21.3
