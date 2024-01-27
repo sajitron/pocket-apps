857785e38820b39c6f18ccfccd9260b278078dc8
Adams Banjo <sajioloye@gmail.com>
2024-01-27 14:43:07 -0500

    set up logger file

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -1,4 +1,26 @@
 package pocketlog
 
+// Logger is used to log information
 type Logger struct {
+	threshold Level
+}
+
+// Debugf formats and prints a message if the log level is debug or higher.
+func (l *Logger) Debugf(format string, args ...any) {
+	// TODO: implement
+}
+
+// Infof formats and prints a message if the log level is info or higher.
+func (l *Logger) Infof(format string, args ...any) {
+	// TODO: implement
+}
+
+// Errorf formats and prints a message if the log level is error or higher.
+func (l *Logger) Errorf(format string, args ...any) {
+	// TODO: implement
+}
+
+// New returns a logger, ready to log at the required threshold
+func New(threshold Level) *Logger {
+	return &Logger{threshold: threshold}
 }
