df8800c684b0fcf6eb7517d33a69697bc4a4d7e1
Adams Banjo <sajioloye@gmail.com>
2024-02-11 19:58:33 -0500

    refactor logger to accept output type

--- logger/pocketlog/logger.go
+++ logger/pocketlog/logger.go
@@ -1,40 +1,61 @@
 package pocketlog
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 // Logger is used to log information
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // Debugf formats and prints a message if the log level is debug or higher.
 func (l *Logger) Debugf(format string, args ...any) {
-	if l.threshold > LevelDebug {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	_, _ = fmt.Printf(format+"\n", args...)
+	if l.threshold <= LevelDebug {
+		l.logf(format, args...)
+	}
 }
 
 // Infof formats and prints a message if the log level is info or higher.
 func (l *Logger) Infof(format string, args ...any) {
-	if l.threshold > LevelInfo {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	_, _ = fmt.Printf(format+"\n", args...)
+	if l.threshold <= LevelInfo {
+		l.logf(format, args...)
+	}
 }
 
 // Errorf formats and prints a message if the log level is error or higher.
 func (l *Logger) Errorf(format string, args ...any) {
-	if l.threshold > LevelError {
-		return
+	if l.output == nil {
+		l.output = os.Stdout
 	}
 
-	_, _ = fmt.Printf(format+"\n", args...)
+	if l.threshold <= LevelError {
+		l.logf(format, args...)
+	}
 }
 
 // New returns a logger, ready to log at the required threshold
-func New(threshold Level) *Logger {
-	return &Logger{threshold: threshold}
+// The default output is Stdout
+func New(threshold Level, output io.Writer) *Logger {
+	return &Logger{
+		threshold: threshold,
+		output:    output,
+	}
+}
+
+// logf prints the message to the output.
+// Add decorations here, if any.
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
--- logger/pocketlog/logger_test.go
+++ logger/pocketlog/logger_test.go
@@ -1,9 +1,13 @@
 package pocketlog_test
 
-import "github.com/sajitron/pocket-apps/logger/pocketlog"
+import (
+	"os"
+
+	"github.com/sajitron/pocket-apps/logger/pocketlog"
+)
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
 }
