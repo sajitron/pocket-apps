053cdc6f5dd26e9dcfcb382b2c6bc3b17fdec336
Adams Banjo <sajioloye@gmail.com>
2024-02-18 17:21:27 -0500

    update logger test

--- logger/pocketlog/logger_test.go
+++ logger/pocketlog/logger_test.go
@@ -2,12 +2,67 @@ package pocketlog_test
 
 import (
 	"os"
+	"testing"
 
 	"github.com/sajitron/pocket-apps/logger/pocketlog"
 )
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, pocketlog.WithOutput(os.Stdout))
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
+}
+
+// testWriter is a struct that implements the io.Writer interface.
+// We use it to validate that we can write to a specific output.
+type testWriter struct {
+	contents string
+}
+
+func (tw *testWriter) Write(p []byte) (n int, err error) {
+	tw.contents = tw.contents + string(p)
+	return len(p), nil
+}
+
+const (
+	debugMessage = "Why write I still all one, ever the same,"
+	infoMessage  = "And keep invention in a noted weed,"
+	errorMessage = "That every word doth almost tell my name,"
+)
+
+func TestLogger_DebugfInfofErrorf(t *testing.T) {
+	type testCase struct {
+		level    pocketlog.Level
+		expected string
+	}
+
+	tt := map[string]testCase{
+		"debug": {
+			level:    pocketlog.LevelDebug,
+			expected: debugMessage + "\n" + infoMessage + "\n" + errorMessage + "\n",
+		},
+		"info": {
+			level:    pocketlog.LevelInfo,
+			expected: infoMessage + "\n" + errorMessage + "\n",
+		},
+		"error": {
+			level:    pocketlog.LevelError,
+			expected: errorMessage + "\n",
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			tw := &testWriter{}
+
+			testedLogger := pocketlog.New(tc.level, pocketlog.WithOutput(tw))
+			testedLogger.Debugf(debugMessage)
+			testedLogger.Infof(infoMessage)
+			testedLogger.Errorf(errorMessage)
+
+			if tw.contents != tc.expected {
+				t.Errorf("invalid contents, expected %q, got %q", tc.expected, tw.contents)
+			}
+		})
+	}
 }
